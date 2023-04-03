b08639f7cc92abcdbb54af60f592e4298abd7cd7
fadale <fadale2011117@hotmail.com>
2023-04-03 22:03:51 +0300

    Initial commit

---
+++ card/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.views.generic import TemplateView
+from . import views
+urlpatterns = [
+    path("", views.CardListView.as_view(), name="card-list"),
+    path("new", views.CardCreateView.as_view(), name="card-create"),
+    path("edit/<int:pk>", views.CardUpdateView.as_view(), name="card-update"),
+    path("delete/<int:pk>", views.CardDeleteView.as_view(), name="card-delete"),
+    path("box/<int:box_num>", views.BoxView.as_view(), name="box"),
+    path("delete/<int:pk>", views.CardDeleteView.as_view(), name="card-delete"),
+]
